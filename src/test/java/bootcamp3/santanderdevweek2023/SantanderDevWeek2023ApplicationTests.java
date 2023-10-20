544b4c87bf4f9ec6523e5189c4f8ad40069047b8
lfmluisf <93626373+lfmluisf@users.noreply.github.com>
2023-10-20 14:07:23 -0300

    Initial commit

---
+++ src/test/java/bootcamp3/santanderdevweek2023/SantanderDevWeek2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp3.santanderdevweek2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
